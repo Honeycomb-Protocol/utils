eece101b2a9e9e963d21dbd43cadc353c9be2129
Ghulam Mustafa Raza Qadri <ghulammustafasoomar@gmail.com>
2023-11-24 14:00:35 +0000

    dep update

--- src/collection.rs
+++ src/collection.rs
@@ -10,53 +10,54 @@ use {
 };
 
 pub fn approve<'info>(
-    collection_mint: AccountInfo<'info>,
-    collection_metadata: AccountInfo<'info>,
-    update_authority: AccountInfo<'info>,
-    new_authority: AccountInfo<'info>,
-    new_authority_record: AccountInfo<'info>,
-    system_program: AccountInfo<'info>,
-    rent_program: AccountInfo<'info>,
-    payer: AccountInfo<'info>,
-    signer_seeds: Option<&[&[&[u8]]; 1]>,
+    _collection_mint: AccountInfo<'info>,
+    _collection_metadata: AccountInfo<'info>,
+    _update_authority: AccountInfo<'info>,
+    _new_authority: AccountInfo<'info>,
+    _new_authority_record: AccountInfo<'info>,
+    _system_program: AccountInfo<'info>,
+    _rent_program: AccountInfo<'info>,
+    _payer: AccountInfo<'info>,
+    _signer_seeds: Option<&[&[&[u8]]; 1]>,
 ) -> Result<()> {
-    let (collection_authority_record, _) =
-        find_collection_authority_account(&collection_mint.key(), &update_authority.key());
-    if new_authority_record.key() != collection_authority_record {
-        return Err(UtilErrors::InvalidNewAuthorityRecord.into());
-    }
-    let mut binding = ApproveCollectionAuthorityBuilder::new();
-    let verify_builder = binding
-        .collection_authority_record(collection_authority_record)
-        .new_collection_authority(new_authority.key())
-        .update_authority(update_authority.key())
-        .payer(payer.key())
-        .metadata(collection_metadata.key())
-        .mint(collection_mint.key())
-        .system_program(system_program.key())
-        .rent(rent_program.key());
+    return Ok(());
+    // let (collection_authority_record, _) =
+    //     find_collection_authority_account(&collection_mint.key(), &update_authority.key());
+    // if new_authority_record.key() != collection_authority_record {
+    //     return Err(UtilErrors::InvalidNewAuthorityRecord.into());
+    // }
+    // let mut binding = ApproveCollectionAuthorityBuilder::new();
+    // let verify_builder = binding
+    //     .collection_authority_record(collection_authority_record)
+    //     .new_collection_authority(new_authority.key())
+    //     .update_authority(update_authority.key())
+    //     .payer(payer.key())
+    //     .metadata(collection_metadata.key())
+    //     .mint(collection_mint.key())
+    //     .system_program(system_program.key())
+    //     .rent(rent_program.key());
 
-    let verify_ix = verify_builder.build().unwrap().instruction();
+    // let verify_ix = verify_builder.build().unwrap().instruction();
 
-    let mut account_infos = vec![
-        new_authority_record,
-        new_authority,
-        update_authority,
-        payer,
-        collection_metadata,
-        collection_mint,
-        system_program,
-        rent_program,
-    ];
+    // let mut account_infos = vec![
+    //     new_authority_record,
+    //     new_authority,
+    //     update_authority,
+    //     payer,
+    //     collection_metadata,
+    //     collection_mint,
+    //     system_program,
+    //     rent_program,
+    // ];
 
-    if let Some(signer_seeds) = signer_seeds {
-        return solana_program::program::invoke_signed(
-            &verify_ix,
-            &account_infos[..],
-            signer_seeds,
-        )
-        .map_err(Into::into);
-    } else {
-        return solana_program::program::invoke(&verify_ix, &account_infos[..]).map_err(Into::into);
-    }
+    // if let Some(signer_seeds) = signer_seeds {
+    //     return solana_program::program::invoke_signed(
+    //         &verify_ix,
+    //         &account_infos[..],
+    //         signer_seeds,
+    //     )
+    //     .map_err(Into::into);
+    // } else {
+    //     return solana_program::program::invoke(&verify_ix, &account_infos[..]).map_err(Into::into);
+    // }
 }
